bcec018c0b223087955c399eeef7d7a26951366a
Li Xulun <lixulun99@hotmail.com>
2020-02-22 13:35:14 +0800

    Add: enhance test command with test all module.

--- lun_fun/test.py
+++ lun_fun/test.py
@@ -5,9 +5,12 @@ from . import main_commands
 from .misc import load_object
 
 @main_commands.command()
-@click.argument('module_name')
+@click.argument('module_name', default="*")
 def test(module_name):
     """
     测试指定模块
     """
-    unittest.main(module="lun_fun." + module_name, argv=[sys.argv[0]])
+    mod_path = "lun_fun." + module_name
+    if module_name == "*":
+        mod_path = "lun_fun"
+    unittest.main(module=mod_path, argv=[sys.argv[0]])
