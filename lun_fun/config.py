9eafa0593714c8656b832ac78b1af8f94f0f0aeb
Li Xulun <lixulun99@hotmail.com>
2020-06-17 11:24:17 +0800

    fixed configparser backports

--- lun_fun/config.py
+++ lun_fun/config.py
@@ -4,4 +4,5 @@ import pathlib
 path_ini = pathlib.Path.home()/'lun-fun.ini'
 
 config = configparser.ConfigParser()
-config.read(path_ini, encoding='utf8')
+# 使用List参数是常规用法，单文件名不能兼容以前版本的configparser
+config.read([path_ini], encoding='utf8')
